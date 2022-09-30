d94e1ba175963d1c47189eb2e2d5317a86ba0642
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-09-30 10:28:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace demo1
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            student s = new student();
+
+            s.Marksscored = 400;
+            s.Studentid = 123;
+            s.StudentName = "sahil";
+            s.Standard = 10;
+            s.StudentAttended = 220;
+            s.TotalMarks = 600;
+            s.TotalWorkingDays = 250;
+
+            
+            Console.WriteLine("student name:"+s.StudentName);
+            Console.WriteLine("student id:"+s.Studentid);
+            Console.WriteLine("student attended:"+s.StudentAttended);
+            Console.WriteLine("standard:"+s.Standard);
+            Console.WriteLine("totalmarks:"+s.TotalMarks);
+            Console.WriteLine("totalworkingdays:"+s.TotalWorkingDays);
+            Console.WriteLine("totalmarksScored:"+s.Marksscored);
+           Console.WriteLine("Result:"+s.Result);
+
+            s.Percentage();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace demo1
+{
+    internal class student
+    {
+         private int studentid;
+         private string studentName;
+         private int totalMarks;
+         private int totalWorkingDays;
+         private  int standard;
+         private int studentAttended;
+         private  int marksscored;
+         public int Result;
+
+      
+        public int Studentid
+        {
+            get { return studentid; }
+            set { studentid = value; }
+           
+        }
+        public string StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        
+        }
+        
+        
+        public int TotalMarks
+        {
+            get { return totalMarks; }
+            set
+            {
+                totalMarks = value;
+            }
+        }
+        public int TotalWorkingDays
+        {
+            get { return totalWorkingDays; }
+            set
+            {
+                totalWorkingDays = value;
+            }
+           
+        }
+
+        public int Standard
+        {
+            get { return standard; }
+            set
+            {
+                standard = value;
+            }
+           
+        }
+
+        public int StudentAttended
+        {
+            get { return studentAttended; }
+            set
+            {
+                studentAttended = value;
+            }
+        }
+        public int Marksscored
+        {
+            get { return marksscored; }
+            set
+            {
+                marksscored = value;
+            }
+        }
+
+
+        public int Percentage()
+        {
+            Result = (Marksscored / TotalMarks) * 100;
+
+            return Result;
+        }
+    }
+}
