2015be64b37eabda6c1f219a664332ca923325a1
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-10-03 22:51:13 +0530

    Add files via upload
    Inheritance example code

--- Program.cs
+++ Program.cs
@@ -1,34 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace demo1
+namespace Inheritance
 {
     internal class Program
     {
-       
         static void Main(string[] args)
         {
-            
-            student s = new student();
+            Batsman b = new Batsman();  
+            b.play();
 
-            s.Marksscored = 400;
-            s.Studentid = 123;
-            s.StudentName = "sahil";
-            s.Standard = 10;
-            s.StudentAttended = 220;
-            s.TotalMarks = 600;
-            s.TotalWorkingDays = 250;
-
-            
-            Console.WriteLine("student name:"+s.StudentName);
-            Console.WriteLine("student id:"+s.Studentid);
-            Console.WriteLine("student attended:"+s.StudentAttended);
-            Console.WriteLine("standard:"+s.Standard);
-            Console.WriteLine("totalmarks:"+s.TotalMarks);
-            Console.WriteLine("totalworkingdays:"+s.TotalWorkingDays);
-            Console.WriteLine("totalmarksScored:"+s.Marksscored);
-           Console.WriteLine("Result:"+s.Result);
-
-            s.Percentage();
+            Console.WriteLine("Team: "+b.team);
+            Console.WriteLine("Batsman age is: "+b.age);
         }
     }
 }
