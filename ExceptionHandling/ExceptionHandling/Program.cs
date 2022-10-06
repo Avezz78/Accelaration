98a8ae01fec7b62212c00c2e9be839c819129f92
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-10-06 18:24:03 +0530

    Add files via upload

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if user entered zero tat will through an exception to handle tat we r using try catch
+            int Number1, Number2, Result;
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                Number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number:");
+                Number2 = int.Parse(Console.ReadLine());
+                Result = Number1 / Number2; 
+                Console.WriteLine($"Result = {Result}");
+            }
+            catch
+            {
+                Console.WriteLine("cant divide by zero plz enter valid number");
+            }
+            Console.ReadKey();
+        }
+    }
+    }
